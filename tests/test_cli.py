638e3e8df073cec666b34d541f571187f2513c0a
Gregory Fan <gfan1316@gmail.com>
2026-04-14 18:13:54 -0400

    Checking single test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,27 +26,10 @@ async def test_start():
     await pubsub.close()
     await r.close()
 
-@pytest.mark.asyncio
-async def test_wait_for_services():
-    r = Redis(host='localhost', port=6379, decode_responses=True)
-    expected_services = {"test", "test_2"}
-
-    main.await_for_services(r, expected_services)
-
-    await r.sadd("services.ready", "test")
-
-    #Check that the function is still waiting for the second service
-    assert not r.sismember("services.ready", "test_2"), "Function should still be waiting for test_2 to be ready"
-    await r.sadd("services.ready", "test_2")
-
-    #Check that the function has now detected that both services are ready
-    assert r.sismember("services.ready", "test_2"), "Function should have detected that test_2 is ready"
-
-    #cleanup
-    await r.srem("services.ready", "test")
-    await r.srem("services.ready", "test_2")
-    await r.close()
+    
 
 @pytest.mark.asyncio
 async def test_handle_queried_images():
     assert True
+
+
