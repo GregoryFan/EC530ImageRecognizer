50767eb7117d6a97df227c3173142cd6d0314099
Gregory Fan <gfan1316@gmail.com>
2026-04-14 17:56:26 -0400

    pytest marks

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,11 +5,14 @@ from redis.asyncio import Redis
 #r = Redis(host='localhost', port=6379, decode_responses=True)
 #pubsub = r.pubsub()
 
+@pytest.mark.asyncio
 async def test_start():
     assert True
 
+@pytest.mark.asyncio
 async def test_wait_for_services():
     assert True
 
+@pytest.mark.asyncio
 async def test_handle_queried_images():
     assert True
