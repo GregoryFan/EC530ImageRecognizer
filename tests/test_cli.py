2de0ef7d74a011aeda624ad35cdc850857a93862
Gregory Fan <gfan1316@gmail.com>
2026-04-14 17:49:07 -0400

    early tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,15 @@
+import main
+import asyncio
+from redis.asyncio import Redis
 
-def test_temp():
+#r = Redis(host='localhost', port=6379, decode_responses=True)
+#pubsub = r.pubsub()
+
+async def test_start():
+    assert True
+
+async def test_wait_for_services():
+    assert True
+
+async def test_handle_queried_images():
     assert True
